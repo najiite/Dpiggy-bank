54922edbe91e26c4797e1142a022852bc71cce06
Onajite Kelvin <onajiteokeh@gmail.com>
2022-07-14 00:29:46 +0100

    Added withdraw function

--- PiggyBank.sol
+++ PiggyBank.sol
@@ -3,14 +3,21 @@
 pragma solidity ^0.8.0;
 
 contract PiggyBank {
-    address public owner;
+    address payable public owner;
     event deposit(uint amount);
     event withdraw(uint amount);
 
-    constructor(){
-        owner = msg.sender; 
+    constructor() {
+        owner = payable(msg.sender); 
     }
 // withdraw from the piggy bank
+    function withdrawPiggy() external payable {
+        require(msg.sender == owner, "Only contract owner can withdraw");
+        owner.transfer(address(this).balance);
+        emit withdraw(address(this).balance);
+
+    }
+//withdraw and delete the contract
     function breakPiggy()  external {
         require(msg.sender == owner, "Only contract owner can break the piggy");
         emit withdraw(address(this).balance);
